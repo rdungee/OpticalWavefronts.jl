99417a412fd4549cce6fd4627f293a09b6b091a6
Ryan Dungee <ryan.dungee@utoronto.ca>
2023-05-23 16:50:27 -0400

    Basic wavefront w/ coords, assumes square+regular

--- src/OpticalWavefronts.jl
+++ src/OpticalWavefronts.jl
@@ -1,5 +1,26 @@
 module OpticalWavefronts
 
-# Write your package code here.
+export Wavefront
+
+struct Wavefront
+    x::AbstractVector{AbstractFloat}
+    wf::Matrix{Union{Complex,AbstractFloat}}
+end
+
+function Base.size(wf::Wavefront)
+    return size(wf.wf)
+end
+
+function Base.getindex(wf::Wavefront, I::Vararg{Int,2})
+    return wf.wf[I...]
+end
+
+function Base.setindex!(wf::Wavefront, value, I::Vararg{Int,2})
+    return wf.wf[I...] = value
+end
+
+function Base.length(wf::Wavefront)
+    return prod(size(wf.wf))
+end
 
 end
