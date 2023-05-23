181c4eb217664fc135d7fa7ff53d8bbca82eb874
Ryan Dungee <ryan.dungee@utoronto.ca>
2023-05-23 17:11:12 -0400

    Quick test to make sure basic operations work

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,12 @@ using OpticalWavefronts
 using Test
 
 @testset "OpticalWavefronts.jl" begin
-    # Write your tests here.
+    testarr = zeros(Complex, 3, 3)
+    testarr[1,1] = 1.0 + 1.0im
+    x = Wavefront(-10.0:0.5:10.0, testarr)
+    @test size(x) == (3, 3)
+    @test x[1,1] == 1.0 + 1.0im
+    @test length(x) == 9
+    x[1,1] = -1.0 + 1.0im
+    @test x[1,1] == -1.0 + 1.0im
 end
